d996983d3d60c5d906fd1bb8d0ad74015199f637
Ubuntu <ubuntu@ip-172-31-9-79.us-east-2.compute.internal>
2017-09-25 22:02:56 +0000

    fixed my phonenum and now using trial acc

--- netless/netless.py
+++ netless/netless.py
@@ -1,13 +1,13 @@
 import os
 from twilio.rest import Client
 
-acc_sid = os.environ["TASID"]
-acc_tok = os.environ["TATOK"]
+acc_sid = os.environ["ASID"]
+acc_tok = os.environ["ATOK"]
 
 client = Client(acc_sid, acc_tok)
 
 client.messages.create(
-    to="+7909248538",
+    to="+447909248538",
     from_="+447481346184",
     body="Test"
 )
